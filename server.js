33ed4b47037c98e0cfc32f0fab858bc417dfb585
catn8813-hash <catn8813@gmail.com>
2025-12-25 09:27:04 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Site online na porta " + PORT);
+});
